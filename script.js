74006604e8e6d484d190eb8a78e2d2e3308429ce
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2024-08-07 23:15:03 +0700

    created grid resize button and its functions

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
 const container = document.querySelector('.container');
 
-function makeGrid () {
-    for (let i=0; i < 16; i++) {
-        for (let j=0; j < 16; j++) {
+function makeGrid (grid) {
+    for (let i=0; i < grid; i++) {
+        for (let j=0; j < grid; j++) {
             row = document.createElement('div');
             container.appendChild(row);
             row.classList.add('draw');
-            /*row.textContent = `${i},${j}`*/
+            /*row.textContent = `${i},${j}`;*/
         }
+    }
+    draw = document.querySelectorAll('.draw');
+    return draw;
+}
+
+function destroyGrid() {
+    for (let i = 0; i < (initialGrid * initialGrid); i++) {
+        container.removeChild(draw[i]);
     }
 }
 
-makeGrid();
-
-draw = document.querySelectorAll('.draw');
-console.log(draw);
-for (let i =0; i < 256; i++) {
-    draw[i].addEventListener('mouseover', function(e) {
-        console.log('draw clicked');
-        draw[i].style.backgroundColor = 'black';
-    })
+function redraw() {
+    for (let i =0; i < (initialGrid * initialGrid); i++) {
+        draw[i].addEventListener('mouseover', function() {
+            draw[i].style.backgroundColor = 'black';
+        })
+    }
 }
+
+let initialGrid = 16;
+makeGrid(initialGrid);
+redraw();
+
+const gridButton = document.querySelector('.grid-button');
+gridButton.addEventListener('click', function() {
+    let newGrid = prompt('insert the size of the grid: (<=100)' );
+    if (newGrid > 100) {
+        alert('the size must less than 101!');
+    }
+    else if (newGrid <= 100 && newGrid != null) {
+        console.log(newGrid);
+        destroyGrid();
+        makeGrid(newGrid);
+        initialGrid = newGrid;
+        redraw();
+        return initialGrid;
+    }
+    else if(newGrid == null) {;
+        alert("Resize cancelled");
+        return;
+    }
+    else {
+        console.log(newGrid);
+        alert("invalid prompt, please try again.");
+    }
+}
+)
