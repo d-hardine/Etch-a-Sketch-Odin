80615182b9ece69259008a0d4efa5fe479771918
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2024-08-08 15:29:06 +0700

    the assignment finished

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+let drawingSize = 850;
 const container = document.querySelector('.container');
+container.style.width =container.style.height = drawingSize + 'px';
+console.log(container);
 
 function makeGrid (grid) {
-    for (let i=0; i < grid; i++) {
-        for (let j=0; j < grid; j++) {
-            row = document.createElement('div');
-            container.appendChild(row);
-            row.classList.add('draw');
-            /*row.textContent = `${i},${j}`;*/
-        }
+    for (let i=0; i < (grid*grid); i++) {
+        let pixel = document.createElement('div');
+        pixel.style.width = ((drawingSize/grid) -2) + 'px';
+        pixel.style.height = ((drawingSize/grid) -2) + 'px';
+        container.appendChild(pixel);
+        pixel.classList.add('draw');
     }
     draw = document.querySelectorAll('.draw');
     return draw;
-}
+    }
 
 function destroyGrid() {
     for (let i = 0; i < (initialGrid * initialGrid); i++) {
@@ -37,7 +39,7 @@ gridButton.addEventListener('click', function() {
     if (newGrid > 100) {
         alert('the size must less than 101!');
     }
-    else if (newGrid <= 100 && newGrid != null) {
+    else if (newGrid <= 100 && newGrid != null && newGrid != '') {
         console.log(newGrid);
         destroyGrid();
         makeGrid(newGrid);
